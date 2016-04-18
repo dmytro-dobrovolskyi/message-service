3ba7ab91fa607505daad73fcb090caebe54ada98
dmytro-dobrovolskyi <dobrovolskyi.dmytro@gmail.com>
2016-04-18 13:18:15 +0300

    Change Parser API

--- parser/src/main/java/com/ddobrovolskyi/parser/Parser.java
+++ parser/src/main/java/com/ddobrovolskyi/parser/Parser.java
@@ -1,7 +1,7 @@
 package com.ddobrovolskyi.parser;
 
 public interface Parser {
-    <T> T parse(String content);
+    <T> T parse(String content, Class<T> type);
 
     boolean canParse(String content);
 }
